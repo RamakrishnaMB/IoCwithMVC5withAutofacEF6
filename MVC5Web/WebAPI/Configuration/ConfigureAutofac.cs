822e0eb060cd014ad0633e714e118c4c5ea61f4a
ramakrishna.mb <ramakrishna.mb@excelindia.com>
2019-11-19 19:48:30 +0530

    Autofac for webapi configured and webapi controller demo method added

---
+++ MVC5Web/WebAPI/Configuration/ConfigureAutofac.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using BuisnessLayer.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Web.Http;
+
+namespace WebAPI.Configuration
+{
+    public class ConfigureAutofac
+    {
+        public static void RegisterAutofac()
+        {
+
+            var builder = new ContainerBuilder();
+            var config = GlobalConfiguration.Configuration;
+            //below line for web api controllers 
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterAssemblyModules(AppDomain.CurrentDomain.GetAssemblies());
+            // Register dependencies in controllers
+            builder.RegisterApiControllers(typeof(WebApiApplication).Assembly);
+            // Register dependencies in filter attributes
+            builder.RegisterWebApiFilterProvider(config);
+            //Other dependency in Bussiness layer
+            builder.RegisterModule(new BusinessInjectionModule());
+            var container = builder.Build();
+            //below line for web api controllers 
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
--- MVC5Web/WebAPI/Global.asax.cs
+++ MVC5Web/WebAPI/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebAPI.Configuration;
 
 namespace WebAPI
 {
@@ -17,7 +18,9 @@ namespace WebAPI
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            
+            ConfigureAutofac.RegisterAutofac();
+
+
         }
     }
 }
