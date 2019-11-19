14ac7e6e54f956e4c232cac51ac1bffb3b2713e1
ramakrishna.mb <ramakrishna.mb@excelindia.com>
2019-11-19 13:07:03 +0530

    DataLayer reference added to Models class library and result set imported to model to shown in dashboard

---
+++ MVC5Web/Models/FeeDetails.cs
@@ -0,0 +1,13 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public partial class FeeDetails : uspDashboard_Result
+    {
+    }
+}
